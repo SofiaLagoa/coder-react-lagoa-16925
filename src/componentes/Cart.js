16e9db79dac1224624f6b1c415f12b678bbe7786
Sofía Lagoa <lagoasofiaconstanza@gmail.com>
2021-09-21 18:04:53 -0300

    Desafío: CartContext (update2)

--- src/componentes/Cart.js
+++ src/componentes/Cart.js
@@ -1,28 +1,28 @@
-import { createContext, useState, useEffect} from "react"
-export const contexto = createContext();
+import { useContext, useState, useEffect} from "react"
+import contexto from './CartContext'
+import Spinner from 'react-bootstrap/Spinner'
 
 const Cart = () => {
-const [cart, setCart] = useState([])
-const [isLoaded, setIsLoaded] = useState(false);
+const {cart} = useContext(contexto)
+const [isLoaded, setIsLoaded] = useState(false)
 
 useEffect(() =>{
     setIsLoaded(true)
-    setCart(contexto)
 }, [])
 
 if(!isLoaded){
     return (
-        <>
-        <p>Cargando</p>
-        </>
+    <div className="d-flex justify-content-center">
+       <Spinner animation="border" role="status">
+           <span className="visually-hidden">Loading...</span>
+       </Spinner>
+    </div>
     )
 }
 else{
-
 return(
     cart.map((item) => <p>{item.products.id}</p>)
 )
-  
 }
 }
 export default Cart;
